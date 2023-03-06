612bee8e5621691c54a1c31a656c4a9ec233029c
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-03-06 12:49:10 +0600

    class understood & done

---
+++ src/cl-4-class/classes.js
@@ -0,0 +1,15 @@
+var Person = /** @class */ (function () {
+    function Person(na, em, ins) {
+        this.name = na;
+        this.email = em;
+        this.institute = ins;
+    }
+    Person.prototype.personInfo = function () {
+        console.log("".concat(this.name, " ").concat(this.email, " ").concat(this.institute));
+    };
+    return Person;
+}());
+var jakir = new Person('Jakir', 'jakir@gmail.com', 'RUET');
+console.log(jakir);
+var robin = new Person('Robin', 'robin@gmail.com', 'RUET');
+console.log(robin);
---
+++ src/cl-4-class/classes.ts
@@ -0,0 +1,19 @@
+class Person{
+   name: string;
+   email: string;
+   institute: string;
+
+   constructor(na: string, em: string, ins: string) {
+      this.name = na;
+      this.email = em;
+      this.institute = ins;
+   }
+
+   personInfo() {
+      console.log(`${this.name} ${this.email} ${this.institute}`)
+   }
+}
+const jakir = new Person('Jakir', 'jakir@gmail.com', 'RUET');
+console.log(jakir);
+const robin = new Person('Robin', 'robin@gmail.com', 'RUET');
+console.log(robin)
